29adb7e4a57d394c9fe89718a7def2391a49d9df
TechTasa <111124246+TechTasa@users.noreply.github.com>
2024-11-05 15:23:05 +0530

    fix about/services/cart

--- routes/ServerRoutes/aboutus.js
+++ routes/ServerRoutes/aboutus.js
@@ -1,18 +1,52 @@
 const express = require("express");
-const { MongoClient } = require("mongodb");
 const router = express.Router();
-const bcrypt = require("bcrypt");
 const { connect, getCollection } = require("../../db");
-const path = require("path");
+const { ObjectId } = require("mongodb");
+
 (async () => {
   try {
     // Get a reference to the users collection
     const userCollection = await getCollection("users");
-    router.get("/aboutus", (req, res) => {
+    const productsCollection = await getCollection("products");
+    router.get("/aboutus", async (req, res) => {
+
+      const id = req.session.username;
+      let idString = id.toString();
+      // Check if id is a valid 24-character hex string
+      if (!ObjectId.isValid(id)) {
+        return res.status(400).send("Invalid id");
+      }
+      const user = await userCollection.findOne({ _id: new ObjectId(id) });
+      user._id = idString;
+
+      if (!user.cart) {
+        user.cart = [];
+      }
+
+      // Fetch all products in the user's cart
+      const products = await productsCollection
+        .find({ _id: { $in: user.cart } })
+        .toArray();
+
+      let total = 0;
+      products.forEach((product) => (total += product.offer));
+      // console.log(total);
+      let loggedInUser = await userCollection.findOne({
+        _id: new ObjectId(req.session.username),
+      });
+      let cartItemCount = 0;
+      if (req.session.role == "visitor") {
+        cartItemCount = loggedInUser.cart.length;
+      }
+      // console.log(req.session.username);
       // Display the About File
-      res.sendFile(
-        path.join(__dirname, "..", "../", "public", "pages", "about.html")
-      );
+      res.render("server/about", {
+        user: user,
+        total: total,
+        loggedIn: req.session.username ? true : false,
+        logo: loggedInUser,
+        cartCount: cartItemCount,
+      });
     });
   } finally {
   }
--- routes/Visitor/cart.js
+++ routes/Visitor/cart.js
@@ -38,7 +38,7 @@ const { ObjectId } = require("mongodb");
       if (req.session.role == "visitor") {
         cartItemCount = loggedInUser.cart.length;
       }
-      // console.log(loggedInUser,req.session.role);
+      // console.log(req.session.username);
       
       res.render("visitor/cart", {
         user: user,
