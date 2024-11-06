5d0c39bf5d04bd497d1bf11adb1cac95adcb6d74
TechTasa <111124246+TechTasa@users.noreply.github.com>
2024-11-06 21:51:14 +0530

    Fix Career/Contact

--- routes/ServerRoutes/career.js
+++ routes/ServerRoutes/career.js
@@ -1,19 +1,49 @@
 const express = require("express");
-const { MongoClient } = require("mongodb");
 const router = express.Router();
-const bcrypt = require("bcrypt");
 const { connect, getCollection } = require("../../db");
-const path = require("path");
+const { ObjectId } = require("mongodb");
 
 (async () => {
   try {
     // Get a reference to the users collection
     const userCollection = await getCollection("users");
-    router.get("/career", (req, res) => {
-      // Display the Career File
-      res.sendFile(
-        path.join(__dirname, "..", "../", "public", "pages", "career.html")
-      );
+    const productsCollection = await getCollection("products");
+    router.get("/career", async (req, res) => {
+      const products = await productsCollection.find().toArray();
+      const companyUsers = await userCollection.find({}).toArray();
+      const companies = companyUsers.filter((user) => user.role === "company");
+      const users = await userCollection
+        .find({}, { cover: 1, logo: 1 })
+        .toArray();
+      const covers = users
+        .filter((user) => user.role === 'company' && user.cover)
+        .map((user) => user.cover);
+
+      const logos = users.
+      filter((user) => user.role === 'company' && user.logo)
+      .map((user) => user.logo);
+      let loggedInUser = await userCollection.findOne({
+        _id: new ObjectId(req.session.username),
+      });
+      let cartItemCount = 0;
+      if (req.session.role == "visitor") {
+        cartItemCount = loggedInUser.cart.length;
+      }
+
+      
+      
+      
+
+      res.render("server/career", {
+        loggedIn: req.session.username ? true : false,
+        user: req.session,
+        companies: companies,
+        logos: logos,
+        covers: covers,
+        logo: loggedInUser,
+        cartCount: cartItemCount,
+        products: products,
+      });
     });
   } finally {
   }
--- routes/ServerRoutes/contactus.js
+++ routes/ServerRoutes/contactus.js
@@ -1,18 +1,49 @@
 const express = require("express");
-const { MongoClient } = require("mongodb");
 const router = express.Router();
-const bcrypt = require("bcrypt");
 const { connect, getCollection } = require("../../db");
-const path = require("path");
+const { ObjectId } = require("mongodb");
+
 (async () => {
   try {
     // Get a reference to the users collection
     const userCollection = await getCollection("users");
-    router.get("/contactus", (req, res) => {
-      // Display the Contact File
-      res.sendFile(
-        path.join(__dirname, "..", "../", "public", "pages", "contact.html")
-      );
+    const productsCollection = await getCollection("products");
+    router.get("/contactus", async (req, res) => {
+      const products = await productsCollection.find().toArray();
+      const companyUsers = await userCollection.find({}).toArray();
+      const companies = companyUsers.filter((user) => user.role === "company");
+      const users = await userCollection
+        .find({}, { cover: 1, logo: 1 })
+        .toArray();
+      const covers = users
+        .filter((user) => user.role === 'company' && user.cover)
+        .map((user) => user.cover);
+
+      const logos = users.
+      filter((user) => user.role === 'company' && user.logo)
+      .map((user) => user.logo);
+      let loggedInUser = await userCollection.findOne({
+        _id: new ObjectId(req.session.username),
+      });
+      let cartItemCount = 0;
+      if (req.session.role == "visitor") {
+        cartItemCount = loggedInUser.cart.length;
+      }
+
+      
+      
+      
+
+      res.render("server/contact", {
+        loggedIn: req.session.username ? true : false,
+        user: req.session,
+        companies: companies,
+        logos: logos,
+        covers: covers,
+        logo: loggedInUser,
+        cartCount: cartItemCount,
+        products: products,
+      });
     });
   } finally {
   }
